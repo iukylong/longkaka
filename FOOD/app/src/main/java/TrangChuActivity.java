2498ee430fc67264e640f62874828674462641d9
iu ky long <longdevkmt@gmail.com>
2018-01-21 13:33:28 +0700

    create Activity Home

---
+++ FOOD/app/src/main/java/TrangChuActivity.java
@@ -0,0 +1,17 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.longlongtran.food.R;
+
+/**
+ * Created by LongLongTran on 1/21/2018.
+ */
+
+public class TrangChuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_trangchu);
+    }
+}
